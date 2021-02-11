2ebc98905845c057d96d860976478bf3dd6773ae
Paul Serban <paul.serban@netcentric.biz>
2021-02-11 10:38:27 +0200

    05-REST-examples-added

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
-
+const path = require('path');
+const methodOverride = require('method-override')
 const app = express();
 const port = 3000;
+
+const {v4: uuid } = require('uuid');
+uuid();
 
 app.use(express.urlencoded({extended: true})); // this is needed to parse post requests, without it post requests body is `undefined`
 
 app.use(express.json()); // this is needed to parse json payloads
+app.use(methodOverride('_method'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 // app.use((req, res) => {
 //   console.log(`We got a new request: ${req}`);
@@ -16,10 +23,78 @@ app.use(express.json()); // this is needed to parse json payloads
 //   res.send(`Hello, we got your request, this is the response`);
 // });
 
+let comments = [
+  {
+    id: uuid(),
+    username: 'Tod',
+    comment: 'Lorem test comment, this is funny.'
+  },
+  {
+    id: uuid(),
+    username: 'Skyler',
+    comment: 'Lorem test comment, this is funny.'
+  },
+  {
+    id: uuid(),
+    username: 'SkBerBoi',
+    comment: 'TOddy doddy, tests, info lorem comment.'
+  },
+  {
+    id: uuid(),
+    username: 'onlysaywoof',
+    comment: 'Woof woof wof wooooooof lorem test comment, this is funny.'
+  },
+  {
+    id: uuid(),
+    username: 'Toddddd',
+    comment: 'Lorem bingo test comment, kingston this is funny.'
+  }
+]
+
 app.get('/', (req, res) => {
   res.send('this is the homepage');
 });
 
+app.get('/comments', (req, res) => {
+  res.render('comments/index', { comments });
+})
+
+app.get('/comments/new', (req, res) => {
+  res.render('comments/new');
+})
+
+app.post('/comments', (req, res) => {
+  const { username, comment } = req.body;
+  comments.push({username, comment, id:uuid()});
+  res.redirect('/comments');
+})
+
+app.get('/comments/:id', (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find(c => c.id === id);
+  res.render('comments/show', { comment });
+})
+
+app.patch('/comments/:id', (req, res) => {
+  const { id } = req.params;
+  const newCommentText = req.body.comment;
+  const foundComment = comments.find(c => c.id === id);
+  foundComment.comment = newCommentText;
+  res.redirect('/comments');
+})
+
+app.delete('/comments/:id', (req, res) => {
+  const { id } = req.params;
+  comments = comments.filter(c => c.id !== id);
+  res.redirect('/comments');
+})
+
+app.get('/comments/:id/edit', (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find(c => c.id === id);
+  res.render('comments/edit', { comment });
+
+});
 
 app.get('/r/:subreddit', (req, res) => {
   const { subreddit } = req= req.params;
